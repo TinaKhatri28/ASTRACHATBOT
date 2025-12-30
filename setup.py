46f13f1a1b293bd79cc841c0f6123b0c7570b4d5
Tina Khatri <tinakhatri05@gmail.cm>
2025-12-30 11:46:56 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup( 
+    name =  "ASTRA AI Assistant",
+    version = "0.0.0",
+    author = "Tina Khatri",
+    author_email = "tinakhatri05@gmail.com",
+    packages =  find_packages(),
+    install_requires =  [],
+)
