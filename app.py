1344704a8bf87f630061b39c3276f4df4c323626
Tina Khatri <tinakhatri05@gmail.cm>
2026-02-19 21:30:05 +0530

    path fixed

--- app.py
+++ app.py
@@ -119,5 +119,8 @@ def chat():
 
 
 # -------------------- RUN --------------------
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
