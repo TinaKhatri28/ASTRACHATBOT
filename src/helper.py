b09f4bbca615601b0b258064bec14084b7a5cc11
Tina Khatri <tinakhatri05@gmail.cm>
2026-06-15 21:10:02 +0530

    Optimize embeddings to use Inference API to fix Render Out-Of-Memory error

--- src/helper.py
+++ src/helper.py
@@ -1,7 +1,8 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
+import os
 
 def load_pdf_file(Data):
     loader = DirectoryLoader(Data, glob="*.pdf",loader_cls = PyPDFLoader)
@@ -14,5 +15,8 @@ def text_split(extracted_data):
     return text_chunks
 
 def download_hugging_face_embeddings():
-    embeddings = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-MiniLM-L6-v2")
+    embeddings = HuggingFaceInferenceAPIEmbeddings(
+        api_key=os.environ.get("HUGGINGFACE_API"),
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
     return embeddings
